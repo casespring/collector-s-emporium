3fb0c3ebd0aa0dceca939ee138e93ac0981acd9b
Marco Upia <markremembers@gmail.com>
2024-02-05 13:04:47 -0500

    working and trying to figure out firebase

--- client/collectors_social_media_app/lib/main.dart
+++ client/collectors_social_media_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:collectors_social_media_app/widgets/loading.dart';
+import 'package:collectors_social_media_app/widgets/something_went_wrong.dart';
 import 'package:flutter/material.dart';
+import 'package:collectors_social_media_app/pages/landing_page.dart';
 import 'package:collectors_social_media_app/pages/community_page.dart';
 import 'package:collectors_social_media_app/pages/message_page.dart';
 import 'package:collectors_social_media_app/pages/post_page.dart';
@@ -8,8 +11,11 @@ import 'package:collectors_social_media_app/pages/feed_page.dart';
 import 'package:collectors_social_media_app/pages/profile_page.dart';
 import 'package:collectors_social_media_app/pages/notification_page.dart';
 import 'package:collectors_social_media_app/pages/settings_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -18,19 +24,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/', // the initial route
-      routes: {
-        '/': (context) => const HomePage(), 
-        '/feed': (context) => const FeedPage(), 
-        '/search': (context) => const SearchPage(),
-        '/post': (context) => const PostPage(),
-        '/community': (context) => const CommunityPage(),
-        '/message': (context) => const MessagePage(),
-        '/profile': (context) => const ProfilePage(),
-        '/notification': (context) => const NotificationPage(),
-        '/settings': (context) => const SettingsPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong(errorMessage: snapshot.error.toString());
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/', // the initial route
+            routes: {
+              '/': (context) => LandingPage(), 
+              '/feed': (context) => const FeedPage(), 
+              '/search': (context) => const SearchPage(),
+              '/post': (context) => const PostPage(),
+              '/community': (context) => const CommunityPage(),
+              '/message': (context) => const MessagePage(),
+              '/profile': (context) => const ProfilePage(),
+              '/notification': (context) => const NotificationPage(),
+              '/settings': (context) => const SettingsPage(),
+            },
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
       },
     );
   }
